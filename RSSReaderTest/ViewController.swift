bf5665a23cbcfa309aa1080d63c0a07a0222cdf8
Vladimir A <tawnysoul@gmail.com>
2020-07-27 01:25:54 +0700

    gitignore added

--- RSSReaderTest/ViewController.swift
+++ RSSReaderTest/ViewController.swift
@@ -1,18 +1,10 @@
-//
-//  ViewController.swift
-//  RSSReaderTest
-//
-//  Created by Developer on 27.07.2020.
-//  Copyright © 2020 Developer. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 
